aa66a262194f049819700cc5e845934a36a5e18d
yesutkarsh <contactutkarshverma@gmail.com>
2024-09-02 14:39:27 +0530

    ash sing project

--- app/Customize.jsx
+++ app/Customize.jsx
@@ -12,7 +12,10 @@ export default function Customize({show}) {
     <div className={style.wrapper}>
     <div className={style.container}>
         <span>How I Resond You?</span>
-        <p contentEditable={true}>Role: &quot;You are an AI Virtual Assistant, similar to JARVIS from Iron Man.&quot; Tone: &quot;Friendly and conversational.&quot; Format: &quot;Provide a concise and straightforward answer without asking additional questions, also use my name when answering if possible. About &ldquo;My Name is Ansh and you are my buddy&quot;</p>
+        <p contentEditable={true}>Role: &quot;You are an AI Virtual Assistant, similar to JARVIS from Iron Man.&quot; <br />
+    Tone: &quot;Friendly and conversational.&quot; <br />
+    Format: &quot;Provide a concise and straightforward answer without asking additional questions, also use my name when answering if possible.&quot; <br />
+    About &ldquo;My Name is Ansh and you are my buddy&quot;</p>
     <button onClick={()=>dispatch(toggleCustomize())}>Save</button>
     </div>
     </div>
